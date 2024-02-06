0b83349e8316d8ab0796c95abd85d525f976cba9
Paul Stead <paul.stead@tyke.co.uk>
2024-02-06 13:30:34 +0000

    TRAV-11 Change to shortened tag names

--- src/test/java/uk/co/tyke/travellog/cucumber/StepDefinitions.java
+++ src/test/java/uk/co/tyke/travellog/cucumber/StepDefinitions.java
@@ -5,20 +5,23 @@ import io.cucumber.java.en.*;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.json.JSONArray;
 import org.json.JSONObject;
-import uk.co.tyke.travellog.journey.model.Trip;
-import uk.co.tyke.travellog.journey.model.TripPoint;
+import org.junit.jupiter.api.Assertions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.co.tyke.travellog.journey.model.Location;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
 
 public class StepDefinitions {
 
     private static final String BASE_URL = "http://localhost:8080/journey";
 
     private static Response response;
+
+    private Logger logger = LoggerFactory.getLogger(StepDefinitions.class);
 
     @Given("A journey has been completed")
     public void a_journey_has_been_completed() {
@@ -33,25 +36,50 @@ public class StepDefinitions {
         request.header("Content-Type", "application/json");
 
         JSONObject requestBody = new JSONObject();
-        requestBody.put("name", "Route to nowhere");
-        requestBody.put("notes", "It's a long long way");
+        requestBody.put("na", "Route to nowhere");
+        requestBody.put("no", "It's a long long way");
 
-        TripPoint start = new TripPoint(BigDecimal.valueOf(53.9628), BigDecimal.valueOf(2.0163)
+        Location start = new Location(BigDecimal.valueOf(53.9628), BigDecimal.valueOf(2.0163)
         , 100, LocalDateTime.now());
-        TripPoint finish = new TripPoint(BigDecimal.valueOf(51.1472), BigDecimal.valueOf(2.0475)
+        Location finish = new Location(BigDecimal.valueOf(51.1472), BigDecimal.valueOf(2.0475)
                 , 105, LocalDateTime.now());
 
-        Trip trip = new Trip(Arrays.asList(start, finish));
+        JSONObject startPoint = new JSONObject();
+        startPoint = new JSONObject();
+        startPoint.put("la", BigDecimal.valueOf(53.9628));
+        startPoint.put("lg", BigDecimal.valueOf(2.0163));
+        startPoint.put("al", 100);
+        startPoint.put("ti", LocalDateTime.now());
 
-        requestBody.put("trips", Arrays.asList(trip));
+        JSONObject endPoint = new JSONObject();
+        endPoint = new JSONObject();
+        endPoint.put("la", BigDecimal.valueOf(51.1472));
+        endPoint.put("lg", BigDecimal.valueOf(2.0475));
+        endPoint.put("al", 105);
+        endPoint.put("ti", LocalDateTime.now());
+
+        JSONArray locations = new JSONArray();
+        locations.put(startPoint);
+        locations.put(endPoint);
+
+        JSONObject Leg = new JSONObject();
+        Leg.put("los", locations);
+
+
+        JSONArray legs = new JSONArray();
+        legs.put(Leg);
+
+        requestBody.put("les", legs);
+
+        logger.debug(requestBody.toString());
 
         response = request.body(requestBody.toString())
-                .post("/");
+                .post("");
     }
     @Then("I will see a HTTP No Content response")
     public void i_will_see_a_HTTP_No_Content_response() {
         // Check the result of the save
-//        throw new io.cucumber.java.PendingException();
+        Assertions.assertEquals(201, response.getStatusCode(), "Expected HTTP Create (201)");
     }
 
 }
